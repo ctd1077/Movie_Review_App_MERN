aea16dd504227a1d0b39890923ab4a3319aca917
CamBam <cambam@cambam.com>
2023-09-30 17:11:21 -0600

    movie.js frontend update

--- frontend/src/components/movie.js
+++ frontend/src/components/movie.js
@@ -1,11 +1,40 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import MovieDataService from '../services/movies'
+import { Link } from 'react-router-dom'
 
-function Movie() {
+const Movie = props => {
+
+    const [movie, setMovie] = useState({
+        id: null,
+        title: "",
+        rated: "",
+        reviews:[]
+    })
+
+    const getMovie = id =>{
+        MovieDataService.get(id).then(response => {
+            setMovie(response.data)
+            console.log(response.data)
+        }).catch(e =>{
+            console.log(e)
+        })      
+    }
+
+    useEffect(()=>{
+        getMovie(props.match.params.id)
+    },[props.match.parmas.id])
     return (
-        <div className="App">
-            Movie
+        <div>
         </div>
     )
 }
+
+// function Movie() {
+//     return (
+//         <div className="App">
+//             Movie
+//         </div>
+//     )
+// }
 
 export default Movie;
