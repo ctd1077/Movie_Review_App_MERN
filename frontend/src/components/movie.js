b1d7c59de13b3fa1414aacd6fdd9ea07bbaeabea
CamBam <ctd1077@gmail.com>
2023-10-01 15:48:13 -0600

    frontend movie.ja update cards

--- frontend/src/components/movie.js
+++ frontend/src/components/movie.js
@@ -2,6 +2,15 @@ import React, {useState, useEffect} from "react"
 import MovieDataService from '../services/movies'
 import { Link } from 'react-router-dom'
 
+import Card from 'react-bootstrap/Card';
+import Container  from "react-bootstrap/Container";
+import Image from 'react-bootstrap/Image'
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Media from 'react-bootstrap/Media'
+
+
 const Movie = props => {
 
     const [movie, setMovie] = useState({
@@ -25,17 +34,31 @@ const Movie = props => {
     },[props.match.parmas.id])
     return (
         <div>
+            <Container>
+                <Row>
+                    <Col>
+                    <Image src={movie.poster+"/100px250"} fluid />
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Header as="h5">{movie.title}</Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                    {movie.plot}
+                                </Card.Text>
+                                {props.user &&
+                                <Link to ={"/movies" + props.match.params.id + "/review"}>
+                                    Add Review
+                                    </Link>}
+                            </Card.Body>
+                        </Card>
+                        <br></br>
+                        <h2>Reviews</h2>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
-
-// Git configuration Update test
-// function Movie() {
-//     return (
-//         <div className="App">
-//             Movie
-//         </div>
-//     )
-// }
 
 export default Movie;
