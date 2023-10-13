60e5069159ccc94dcb3a1ae78d1f8c808fbd2d3a
CamBam <ctd1077@gmail.com>
2023-10-12 19:46:51 -0600

    Updated login.js

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,9 +1,54 @@
-import React from "react"
+import React, {useState} from "react";
+import Form from 'react-bootstrap/Form';
 
-function Login() {
+import Button from "react-bootstrap/esm/Button";
+// or this oneimport Button from "react-bootstrap/Button";
+
+const Login = props => {
+
+    const [name, setName] = useState("")
+    const [id, setId] = useState("")
+
+    const onChangeName = e => {
+        const name = e.target.value
+        setName(name);
+    }
+
+    const onChangeId = e => {
+        const id = e.targetvalue
+        setId(id);
+    }
+
+    const login = () => {
+        props.login({name: name, id: id})
+        props.history.push('/')
+    }
+
     return(
-        <div className="App">
-            Login
+        <div>
+            <Form>
+                <Form.Group>
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                    type="text"
+                    placeholder="Enter Name"
+                    value={name}
+                    onChange={onChangeName}
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>ID</Form.Label>
+                    <Form.Control
+                    type="text"
+                    placeholder="Enter id"
+                    value={id}
+                    onChange={onChangeId}
+                    />
+                </Form.Group>
+                <Button variant="primary" onClick={login}>
+                    Submit
+                </Button>
+            </Form>
         </div>
     )
 }
